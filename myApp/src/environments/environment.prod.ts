81750cc6fbb9374b7e0b0a734e77e630ba5d6eda
Mileidys Agamez <agamezmileidys@gmail.com>
2025-11-26 23:44:49 -0500

    configurar variables de entorno

--- myApp/src/environments/environment.prod.ts
+++ myApp/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+     firebaseConfig: {
+  apiKey: "AIzaSyCaYHIcIx2aJNDaVefhvDcfprbNN9Sk7ik",
+  authDomain: "tallerar-4a628.firebaseapp.com",
+  projectId: "tallerar-4a628",
+  storageBucket: "tallerar-4a628.firebasestorage.app",
+  messagingSenderId: "588112001540",
+  appId: "1:588112001540:web:fcc59b74f195651ac25351"
+},
+ supabaseUrl: "https://veydnrzpkxombrjsqqhg.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZleWRucnpwa3hvbWJyanNxcWhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxOTg5NTEsImV4cCI6MjA3OTc3NDk1MX0.JB3tUJVj33YLCUGk5ZAP1H2LM6xl2AKAOwyg1aDXslg"
 };
+
